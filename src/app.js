7f5832109aa2620a15032ae06275c7a06daa09f9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-10-19 16:10:43 +0530

    Add support for uploading files to Cloudinary

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
 
 app.use(express.json())
+app.use(fileUpload())
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 import dotenv from "dotenv";
+import { cloudinaryConnect } from "./utils/cloudinary.js";
 
 dotenv.config({
     path: "./.env",
@@ -12,6 +13,12 @@ connectDB()
             console.log(`ERR: ${error.message}`);
             process.exit(1);
         });
+
+        // return promise to chain Cloudinary Connection
+        return cloudinaryConnect();
+    })
+    .then(() => {
+        console.log(`Cloudinary Connected Successfully !!`);
 
         app.listen(process.env.PORT, () => {
             console.log(`Server is listening at PORT: ${process.env.PORT}`);
